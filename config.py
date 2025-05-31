3ac4b823566adc736c8be6cd2b6bdf80be121e12
MUFAZ VK <93363575+BXBotz2021@users.noreply.github.com>
2025-05-31 21:55:16 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,19 +5,19 @@ BOT_TOKEN = "123456:ABC-DEF1234ghIkl-zyx57W2v1u123ew11"  # bot token
 
 
 # REDIS
-HOST = "localhost"  # redis host uri
+HOST = "redis-16415.c228.us-central1-1.gce.redns.redis-cloud.com"  # redis host uri
 PORT = 6379  # redis port
-PASSWORD = ""  # redis password
+PASSWORD = "lpyhkguL4VmjkU8hyF8MHmbzmsDbE7RL"  # redis password
 
-PRIVATE_CHAT_ID = -1001234567890  # CHAT WHERE YOU WANT TO STORE VIDEOS
+PRIVATE_CHAT_ID = -1002342359173  # CHAT WHERE YOU WANT TO STORE VIDEOS
 # COOKIE FOR AUTHENTICATION (get from chrome dev tools) ex: "PANWEB=1; csrfToken=;
 COOKIE = ""
-ADMINS = [1317173146]
+ADMINS = [6974737899]
 
 
-BOT_USERNAME = "teraboxdown_bot"
+BOT_USERNAME = "teraboxresurebot"
 
 # Force user to join this channel. (make sure you have promoted the bot on this chat.)
-FORCE_LINK = "@RoldexVerse"
+FORCE_LINK = "@MOVIEZ_BOTZ"
 
 PUBLIC_EARN_API = ""  # https://publicearn.com/api
