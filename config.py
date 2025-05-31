05e70bc7193fa90c23439609bd43d30ddab4cf0d
MUFAZ VK <93363575+BXBotz2021@users.noreply.github.com>
2025-05-31 22:11:31 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,9 +5,9 @@ BOT_TOKEN = "123456:ABC-DEF1234ghIkl-zyx57W2v1u123ew11"  # bot token
 
 
 # REDIS
-HOST = "redis-16415.c228.us-central1-1.gce.redns.redis-cloud.com"  # redis host uri
+HOST = "redis-11494.c283.us-east-1-4.ec2.redns.redis-cloud.com:11494"  # redis host uri
 PORT = 6379  # redis port
-PASSWORD = "lpyhkguL4VmjkU8hyF8MHmbzmsDbE7RL"  # redis password
+PASSWORD = "s1w22apeKIm9aSCCEpWQhN4Go3SDnKya"  # redis password
 
 PRIVATE_CHAT_ID = -1002342359173  # CHAT WHERE YOU WANT TO STORE VIDEOS
 # COOKIE FOR AUTHENTICATION (get from chrome dev tools) ex: "PANWEB=1; csrfToken=;
